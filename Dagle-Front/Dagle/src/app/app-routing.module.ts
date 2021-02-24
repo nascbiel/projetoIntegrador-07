51674046da133836897f66b086cf905f2f125d37
GuilhermeSilva-code <fillipi.fs.silva@gmail.com>
2021-02-24 11:40:00 -0300

    Consumo de API login e cadastro

--- Dagle-Front/Dagle/src/app/app-routing.module.ts
+++ Dagle-Front/Dagle/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -8,7 +10,9 @@ const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
 
 {path:'login',component:LoginComponent},
-{path:'cadastrar',component:CadastrarComponent}
+{path:'cadastrar',component:CadastrarComponent},
+{path:'home',component:HomeComponent},
+{path:'categoria',component:CategoriaComponent}
 
 ];
 
