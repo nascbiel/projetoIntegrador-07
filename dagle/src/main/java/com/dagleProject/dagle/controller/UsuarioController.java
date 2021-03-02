08fe181babf205ff5124e17383c9868f3dfb0e78
LucasAnseloni <lucas.anseloni@outlook.com>
2021-03-02 17:03:21 -0300

    post get produto

--- dagle/src/main/java/com/dagleProject/dagle/controller/UsuarioController.java
+++ dagle/src/main/java/com/dagleProject/dagle/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.dagleProject.dagle.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dagleProject.dagle.model.Usuario;
 import com.dagleProject.dagle.model.UsuarioLogin;
+import com.dagleProject.dagle.repository.UsuarioRepository;
 import com.dagleProject.dagle.service.UsuarioService;
 
 @RestController
@@ -22,6 +26,20 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+    private UsuarioRepository repository;
+	
+	@GetMapping
+    public ResponseEntity<List<Usuario>> GetAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> GetById(@PathVariable long id){
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
 	
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user){
