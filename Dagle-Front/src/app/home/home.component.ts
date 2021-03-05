e56bf25dfae8d8f17c0d7f278107e56b7edd65fa
DaviVieira2210 <davi.vieira205@gmail.com>
2021-03-05 14:13:49 -0300

    Deploy do projeto integrador (Dagle)

---
+++ Dagle-Front/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private rouet: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll (0,0)
+    /*if(environment.token == ''){
+      alert('Sua sessão expirou, faça login novamente')
+      this.router.navigate(['/login'])
+    }*/
+
+  }
+
+  
+
+}
